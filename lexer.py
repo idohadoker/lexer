c93687f2df4b8d15d0ed1bad5ab3a28bc5b197db
idohadoker <idohadoker123@yahoo.com>
2022-12-13 21:11:54 +0200

    second commit

--- lexer.py
+++ lexer.py
@@ -76,7 +76,7 @@ def openfile(path: str):
     return f
 
 
-# changes the name of typedef to term before code is being lexer
+# changes the name of typedef to term and define before code is being tokenized
 def find_replace_typedef_define(text: list[str]) -> list[str]:
     global typedef_list
     text_pointer = 0
